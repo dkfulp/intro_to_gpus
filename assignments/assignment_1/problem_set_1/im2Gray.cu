97b86231f3281816160f533772a92dea0169891b
Dakota Fulp <dkfulp@coastal.edu>
2021-02-02 16:54:01 -0500

    Added Serial Version, extra test images, and updated test code

--- assignments/assignment_1/problem_set_1/im2Gray.cu
+++ assignments/assignment_1/problem_set_1/im2Gray.cu
@@ -36,14 +36,9 @@ void im2Gray(uchar4 *d_in, unsigned char *d_grey, int numRows, int numCols){
 
 
 void launch_im2gray(uchar4 *d_in, unsigned char* d_grey, size_t numRows, size_t numCols){
-    // Works but terrible
-    //dim3 grid(numRows,numCols,1);
-  
-    // Ensure that each block doesn't have more than BLOCK threads 
-
     // Ensure there are not over BLOCK number of blocks
     dim3 grid((size_t)((float)BLOCK/2),(size_t)((float)BLOCK/2),1);
-    // Make enough threads per block equal to number of Columns in Image
+    // Given the number of total blocks, determine the number of threads needed per block
     dim3 block((size_t)((float)numCols/((float)BLOCK/2)),(size_t)((float)numRows/((float)BLOCK/2)),1);
     // Call Kernel
     im2Gray<<<grid,block>>>(d_in, d_grey, numRows, numCols);
--- assignments/assignment_1/problem_set_1/main.cpp
+++ assignments/assignment_1/problem_set_1/main.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <cstdio>
 #include <cuda.h>
-#include <cuda_runtime.h> 
+#include <cuda_runtime.h>
 #include <cassert>
-#include <cstdio> 
-#include <string> 
-#include <opencv2/opencv.hpp> 
-
+#include <cstdio>
+#include <string>
+#include <opencv2/opencv.hpp>
 
 #include "im2Gray.h"
-
-
 
 /*
  Driver program to test im2gray
 */
-
 
 /*
  Process input image and allocate memory on host and 
  GPUs.
 */
 
-
 void checkApproxResults(unsigned char *ref, unsigned char *gpu, size_t numElems, float eps){
+    for (int i = 0; i < numElems; i++){
+        if (ref[i] - gpu[i] > eps){
+            std::cerr << "Error at position " << i << "\n";
 
-    for(int i = 0; i < numElems; i++){
-        if(ref[i] - gpu[i] > eps){
-            std::cerr << "Error at position " << i << "\n"; 
-
-            std::cerr << "Reference:: " << std::setprecision(17) << +ref[i] <<"\n";
+            std::cerr << "Reference:: " << std::setprecision(17) << +ref[i] << "\n";
             std::cerr << "GPU:: " << +gpu[i] << "\n";
 
             exit(1);
@@ -37,92 +31,105 @@ void checkApproxResults(unsigned char *ref, unsigned char *gpu, size_t numElems,
     }
 }
 
-
-
 void checkResult(const std::string &reference_file, const std::string &output_file, float eps){
-    cv::Mat ref_img, out_img; 
+    cv::Mat ref_img, out_img;
 
     ref_img = cv::imread(reference_file, -1);
     out_img = cv::imread(output_file, -1);
 
-
     unsigned char *refPtr = ref_img.ptr<unsigned char>(0);
     unsigned char *oPtr = out_img.ptr<unsigned char>(0);
 
-    checkApproxResults(refPtr, oPtr, ref_img.rows*ref_img.cols*ref_img.channels(), eps);
+    checkApproxResults(refPtr, oPtr, ref_img.rows * ref_img.cols * ref_img.channels(), eps);
     std::cout << "PASSED!";
-
-
 }
 
+void im2Gray_serial(uchar4 *h_in, unsigned char *h_grey, int numRows, int numCols){
+    // Iterate over all pixels
+    int x, y;
+    for (x = 0, x < numCols; x++){
+        for (y = 0; y < numRows; y++){
+            // Get pixel location
+            int grey_Offset = y * numCols + x;
+            // Get corresponding rgba pixel at this location
+            uchar4 rgba_pixel = h_in[grey_Offset];
+            // Calculate new grey value
+            h_grey[grey_Offset] = (unsigned char)((float)rgba_pixel.x * 0.299f + (float)rgba_pixel.y * 0.587f + (float)rgba_pixel.z * 0.114f);
+        }
+    }
+}
 
+int main(int argc, char const *argv[]){
+    uchar4 *h_imrgba, *d_imrgba;
+    unsigned char *h_grey, *d_grey;
 
-int main(int argc, char const *argv[])
-{
-   uchar4 *h_imrgba, *d_imrgba;
-   unsigned char *h_grey, *d_grey;
+    std::string infile;
+    std::string outfile;
+    std::string reference;
+    int serial = 0; // 0 for False, 1 for True
 
-   std::string infile; 
-   std::string outfile; 
-   std::string reference = "lena_gray.png";
+    switch (argc){
+    case 2:
+        infile = std::string(argv[1]);
+        outfile = "cinque_gpu_gray.png";
+        reference = "lena_gray.png";
+        break;
+    case 3:
+        infile = std::string(argv[1]);
+        outfile = std::string(argv[2]);
+        reference = "lena_gray.png";
+        break;
+    case 4:
+        infile = std::string(argv[1]);
+        outfile = std::string(argv[2]);
+        reference = std::string(argv[3]);
+        break;
+    default:
+        std::cerr << "Usage ./gray <in_image> <out_image> <reference_image>\n";
+        exit(1);
+    }
 
+    // preprocess
+    cv::Mat img = cv::imread(infile.c_str(), cv::IMREAD_COLOR);
+    cv::Mat imrgba, grayimage;
 
-   switch(argc){
-       case 2:
-            infile = std::string(argv[1]);
-            outfile = "cinque_gpu_gray.png";
-            break; 
-        case 3:
-            infile = std::string(argv[1]);
-            outfile = std::string(argv[2]);
-            break;
-        default: 
-              std::cerr << "Usage ./gray <in_image> <out_image>\n";
-              exit(1);
+    cv::cvtColor(img, imrgba, cv::COLOR_BGR2RGBA);
 
-   }
+    grayimage.create(img.rows, img.cols, CV_8UC1);
 
-   // preprocess 
+    const size_t numPixels = img.rows * img.cols;
 
-   cv::Mat img = cv::imread(infile.c_str(), cv::IMREAD_COLOR);
-   cv::Mat imrgba, grayimage; 
+    h_imrgba = imrgba.ptr<uchar4>(0);
+    h_grey = grayimage.ptr<unsigned char>(0);
 
-   cv::cvtColor(img, imrgba, cv::COLOR_BGR2RGBA);
+    if (serial){
+        im2Gray_serial(h_imrgba, h_grey, img.rows, img.cols);
+    } else {
+        checkCudaErrors(cudaMalloc((void **)&d_imrgba, sizeof(uchar4) * numPixels));
+        checkCudaErrors(cudaMalloc((void **)&d_grey, sizeof(unsigned char) * numPixels));
 
-   grayimage.create(img.rows, img.cols, CV_8UC1);
+        checkCudaErrors(cudaMemcpy(d_imrgba, h_imrgba, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
 
-   const size_t  numPixels = img.rows*img.cols; 
+        // call the kernel
+        launch_im2gray(d_imrgba, d_grey, img.rows, img.cols);
+        cudaDeviceSynchronize();
+        checkCudaErrors(cudaGetLastError());
 
-   h_imrgba = imrgba.ptr<uchar4>(0);
-   h_grey = grayimage.ptr<unsigned char>(0);
+        std::cout << "Finished kernel launch \n";
 
-   checkCudaErrors(cudaMalloc((void**)&d_imrgba, sizeof(uchar4)*numPixels));
-   checkCudaErrors(cudaMalloc((void**)&d_grey, sizeof(unsigned char)*numPixels));
+        checkCudaErrors(cudaMemcpy(h_grey, d_grey, numPixels * sizeof(unsigned char), cudaMemcpyDeviceToHost));
+    }
 
-   checkCudaErrors(cudaMemcpy(d_imrgba, h_imrgba, sizeof(uchar4)*numPixels, cudaMemcpyHostToDevice)); 
+    // create the image with the output data
+    cv::Mat output(img.rows, img.cols, CV_8UC1, (void *)h_grey);
 
+    bool suc = cv::imwrite(outfile.c_str(), output);
+    if (!suc){
+        std::cerr << "Couldn't write image!\n";
+        exit(1);
+    }
 
-   // call the kernel 
-   launch_im2gray(d_imrgba, d_grey, img.rows, img.cols);
-   cudaDeviceSynchronize();
-   checkCudaErrors(cudaGetLastError());
-
-   std::cout << "Finished kernel launch \n";
-
-   checkCudaErrors(cudaMemcpy(h_grey, d_grey, numPixels*sizeof(unsigned char), cudaMemcpyDeviceToHost));
-
-   // create the image with the output data 
-
-   cv::Mat output(img.rows, img.cols, CV_8UC1, (void*)h_grey);
-
-   bool suc = cv::imwrite(outfile.c_str(), output);
-   if(!suc){
-       std::cerr << "Couldn't write image!\n";
-       exit(1);
-   }
-
-
-   // check if the caclulation was correct to a degree of tolerance
+    // check if the caclulation was correct to a degree of tolerance
 
     //checkResult(reference, outfile, 1e-5);
     checkResult(reference, outfile, 2);
@@ -132,6 +139,3 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
-
-
-
