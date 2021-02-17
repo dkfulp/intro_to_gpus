73c36a2385820e950abbeb7d94cbdea5e82bc584
Dakota Fulp <dkfulp@coastal.edu>
2021-02-17 18:57:48 -0500

    Working out small bugs

--- assignments/assignment_2/problem_set_2/main.cpp
+++ assignments/assignment_2/problem_set_2/main.cpp
@@ -161,7 +161,7 @@ int main(int argc, char const *argv[]){
 
     // Gaussian Blur Parameters
     const int fWidth = 9;
-    const float fDev = 1;
+    const float fDev = 3;
 
     // File String Parameters
     std::string infile;
