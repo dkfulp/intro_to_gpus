b6424fe6cee48c440ece9fee79c03a2003313c8a
Dakota Fulp <dkfulp@coastal.edu>
2021-02-20 16:35:24 -0500

    Attempting shared memory

--- assignments/assignment_2/problem_set_2/blur_kernels.cu
+++ assignments/assignment_2/problem_set_2/blur_kernels.cu
@@ -53,10 +53,74 @@ void gaussianBlurGlobal(unsigned char *d_in, unsigned char *d_out, const int num
 // This implementation in specific uses shared memory to reduce the 
 // number of accesses to global memory to improve performance.
  __global__ 
- void gaussianBlurShared(unsigned char *d_in, unsigned char *d_out, const int rows, const int cols, float *d_filter, const int filterWidth){
- 
- 
- } 
+ void gaussianBlurShared(unsigned char *d_in, unsigned char *d_out, const int num_rows, const int num_cols, float *d_filter, const int filterWidth){
+        // Create shared memory input and array
+        __shared__ unsigned char input_pixels[BLOCK*BLOCK];
+
+        // Get location in global memory 
+        int gl_row = blockIdx.y * blockDim.y + threadIdx.y;
+        int gl_col = blockIdx.x * blockDim.x + threadIdx.x;
+
+        // Get location in shared memory
+        int sh_row = threadIdx.y;
+        int sh_col = threadIdx.x;
+
+        // Ensure target working pixel is valid
+        if (gl_col < num_cols && gl_row < num_rows){
+                // Load shared memory values for all interal pixels from global memory
+                int global_offset = gl_row * num_cols + gl_col;
+                int shared_offset = sh_row * blockDim.x + sh_col;
+                input_pixels[shared_offset] = d_in[global_offset];
+        
+                // Make sure all threads have loaded before starting computation
+                __syncthreads();
+
+                // Setup loop variables
+                int in_row, in_col;
+                int in_gl_row, in_gl_col;
+                int blur_sum = 0;
+                int filter_pos = 0;
+
+                // Given the filter width, determine the correct row and col offsets
+                int blur_offset = ((filterWidth-1)/2);
+
+                // Iterate from the furthest back row to the furthest forward row
+                for (in_row = sh_row - blur_offset; in_row <= sh_row + blur_offset; in_row++){
+                        // Iterate from the furthest back col to the furthest forward col
+                        for (in_col = sh_col - blur_offset; in_col <= sh_col + blur_offset; in_col++){
+                                // Target Pixel is In Shared Memory
+                                if (in_row >= 0 && in_row < blockDim.y && in_col >= 0 && in_col < blockDim.x){
+                                        // Get target blur pixel offset
+                                        int shared_offset = in_row * blockDim.x + in_col;
+
+                                        // Multiply current filter location by target pixel and add to running sum
+                                        blur_sum += (int)( (float)input_pixels[shared_offset] * d_filter[filter_pos] );
+                                // Target Pixel is Not In Shared Memory
+                                } else {
+                                        // Ensure target pixel global location is valid
+                                        in_gl_row = blockIdx.y * blockDim.y + in_row;
+                                        in_gl_col = blockIdx.x * blockDim.x + in_col;
+                                        if (in_gl_row >= 0 && in_gl_row < num_rows && in_gl_col >= 0 && in_gl_col < num_cols){
+                                                // Get target blur pixel offset
+                                                int global_offset = in_gl_row * num_cols + in_gl_col;
+
+                                                // Multiply current filter location by target pixel and add to running sum
+                                                blur_sum += (int)( (float)d_in[global_offset] * d_filter[filter_pos] );
+                                        }
+                                }
+                                // Always increment filter location
+                                filter_pos++;
+                        }
+                }
+
+                // Make sure all threads have finished computation
+                __syncthreads();
+
+                // Store results in the correct location of the output array
+                int result_offset = gl_row * num_cols + gl_col;
+                d_out[result_offset] = (unsigned char)blur_sum;
+        } 
+} 
  
 
 
@@ -118,17 +182,20 @@ void gaussianBlurKernel(uchar4* d_imrgba, uchar4 *d_oimrgba, size_t num_rows, si
         checkCudaErrors(cudaGetLastError());
 
         // Compute Gaussian Blur for the red pixel array
-        gaussianBlurGlobal<<<grid, block>>>(d_red, d_rblurred, num_rows, num_cols, d_filter, filterWidth);
+        gaussianBlurShared<<<grid, block>>>(d_red, d_rblurred, num_rows, num_cols, d_filter, filterWidth);
+        //gaussianBlurGlobal<<<grid, block>>>(d_red, d_rblurred, num_rows, num_cols, d_filter, filterWidth);
         cudaDeviceSynchronize();
         checkCudaErrors(cudaGetLastError());
 
         // Compute Gaussian Blur for the green pixel array
-        gaussianBlurGlobal<<<grid, block>>>(d_green, d_gblurred, num_rows, num_cols, d_filter, filterWidth);
+        gaussianBlurShared<<<grid, block>>>(d_green, d_gblurred, num_rows, num_cols, d_filter, filterWidth);
+        //gaussianBlurGlobal<<<grid, block>>>(d_green, d_gblurred, num_rows, num_cols, d_filter, filterWidth);
         cudaDeviceSynchronize();
         checkCudaErrors(cudaGetLastError());
 
         // Compute Gaussian Blur for the blue pixel array
-        gaussianBlurGlobal<<<grid, block>>>(d_blue, d_bblurred, num_rows, num_cols, d_filter, filterWidth);
+        gaussianBlurShared<<<grid, block>>>(d_blue, d_bblurred, num_rows, num_cols, d_filter, filterWidth);
+        //gaussianBlurGlobal<<<grid, block>>>(d_blue, d_bblurred, num_rows, num_cols, d_filter, filterWidth);
         cudaDeviceSynchronize();
         checkCudaErrors(cudaGetLastError());
 
