4e8f35062c538a7286ac42f067337fb183e3b9fd
Dakota Fulp <dkfulp@coastal.edu>
2021-02-27 14:35:24 -0500

    Back to testing seperable row

--- assignments/assignment_2/problem_set_2/main.cpp
+++ assignments/assignment_2/problem_set_2/main.cpp
@@ -240,9 +240,9 @@ int main(int argc, char const *argv[]){
     // Shared V1
     //gaussianBlurKernelSharedv1(d_in_img, d_o_img, img.rows, img.cols, d_red, d_green, d_blue, d_red_blurred, d_green_blurred, d_blue_blurred, d_filter, fWidth);
     // Shared V2
-    gaussianBlurKernelSharedv2(d_in_img, d_o_img, img.rows, img.cols, d_red, d_green, d_blue, d_red_blurred, d_green_blurred, d_blue_blurred, d_filter);
+    //gaussianBlurKernelSharedv2(d_in_img, d_o_img, img.rows, img.cols, d_red, d_green, d_blue, d_red_blurred, d_green_blurred, d_blue_blurred, d_filter);
     // Shared Seperable Row
-    //gaussianBlurKernelSharedSepRow(d_in_img, d_o_img, img.rows, img.cols, d_red, d_green, d_blue, d_red_blurred, d_green_blurred, d_blue_blurred, d_filter, fWidth);
+    gaussianBlurKernelSharedSepRow(d_in_img, d_o_img, img.rows, img.cols, d_red, d_green, d_blue, d_red_blurred, d_green_blurred, d_blue_blurred, d_filter, fWidth);
 
     // Copy the output image from device to host
     checkCudaErrors(cudaMemcpy(h_o_img, d_o_img, sizeof(uchar4) * numPixels, cudaMemcpyDeviceToHost));
