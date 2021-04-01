fef22454a340c6349120ae896bdb1759e1a9cf3c
Dakota Fulp <dkfulp@coastal.edu>
2021-04-01 17:36:09 -0400

    Working on MPI portion

---
+++ assignments/assignment_3/problem_set_3/mpi_test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cassert>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <chrono>
+#include <iomanip>
+#include <cstdlib>
+#include <mpi.h>
+
+int main(int argc, char const *argv[]){
+
+    int id;
+    int ierr;
+    int p;
+    
+    ierr = MPI_Init( &argc, &argv );
+
+    if (ierr != 0){
+        std::cout << "\n";
+        std::cout << "HELLO_MPI - Fatal error!\n";
+        std::cout << "  MPI_Init returned nonzero ierr.\n";
+        exit ( 1 );
+    }
+
+    // Get number of processors
+    ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
+
+    // Get processor id
+    ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
+
+    // Print Statement
+    std::cout << "P" << id << ":    'Hello, world!'\n";
+
+    // Finalize MPI
+    MPI_Finalize ( );
+}
