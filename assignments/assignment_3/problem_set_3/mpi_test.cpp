d66c1f1937d969d11ac845a57ccdbe023ed8475c
Dakota Fulp <dkfulp@coastal.edu>
2021-04-01 22:35:26 -0400

    Working on mpi

--- assignments/assignment_3/problem_set_3/mpi_test.cpp
+++ assignments/assignment_3/problem_set_3/mpi_test.cpp
@@ -322,61 +322,6 @@ int main(int argc, char** argv) {
     // sync up all processes
     MPI_Barrier(MPI_COMM_WORLD);
 
-    if (current_rank == 0){
-        for (int i = 0; i < rank_rows; i++){
-            for (int j = 0; j < num_cols; j++){
-                int location = i * num_cols + j;
-                std::cout << rank_U[location] << "\t";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if (current_rank == 1){
-        for (int i = 1; i < rank_rows+1; i++){
-            for (int j = 0; j < num_cols; j++){
-                int location = i * num_cols + j;
-                std::cout << rank_U[location] << "\t";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if (current_rank == 2){
-        for (int i = 1; i < rank_rows+1; i++){
-            for (int j = 0; j < num_cols; j++){
-                int location = i * num_cols + j;
-                std::cout << rank_U[location] << "\t";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if (current_rank == 3){
-        for (int i = 1; i < rank_rows+1; i++){
-            for (int j = 0; j < num_cols; j++){
-                int location = i * num_cols + j;
-                std::cout << rank_U[location] << "\t";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-
     // Begin Jacobi computation
     /////////////////////////////
 
@@ -479,6 +424,65 @@ int main(int argc, char** argv) {
 
     // sync up all processes
     MPI_Barrier(MPI_COMM_WORLD);
+
+    if (current_rank == 0){
+        for (int i = 0; i < rank_rows+1; i++){
+            for (int j = 0; j < num_cols; j++){
+                int location = i * num_cols + j;
+                std::cout << rank_U[location] << "\t";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (current_rank == 1){
+        for (int i = 0; i < rank_rows+2; i++){
+            for (int j = 0; j < num_cols; j++){
+                int location = i * num_cols + j;
+                std::cout << rank_U[location] << "\t";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (current_rank == 2){
+        for (int i = 0; i < rank_rows+2; i++){
+            for (int j = 0; j < num_cols; j++){
+                int location = i * num_cols + j;
+                std::cout << rank_U[location] << "\t";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (current_rank == 3){
+        for (int i = 0; i < rank_rows+1; i++){
+            for (int j = 0; j < num_cols; j++){
+                int location = i * num_cols + j;
+                std::cout << rank_U[location] << "\t";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+
+
+
+
+
 
     // Compute one iterations of Jacobi
 
